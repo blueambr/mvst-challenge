7e143eb7c5b9dc842b705ca6ea0b64a3a43d4b92
Vlad Gerasimovich <hotepp@icloud.com>
2023-04-17 17:03:24 +0200

    feat: update README, add simple null check for items

--- README.md
+++ README.md
@@ -4,3 +4,28 @@
 
 1. Reflect the [Design](https://www.figma.com/file/eXKRZRFUDXaXEbRq2SnikI/Coding-Challenge?node-id=2-2)
 2. Render two lists (coffee and tea) with the Backend data
+
+### How to run
+
+| Node.js 18.\*
+
+1. `pnpm i` or `npm i`
+2. `pnpm dev` or `npm run dev`
+
+### Implementation details
+
+The task is implemented with Next.js, MongoDB and Tailwind CSS, meaning it is a Headless architecture without a classic Backend setup.
+
+The repo was kickstarted using `pnpm create next-app`.
+
+This is the data structure for `coffee`. It is the same for `tea`, but without `tags`:
+
+![`coffee` data structure](./GitHub/coffee-data-structure.png)
+
+### Things I would do next
+
+- Test, that we receive data and that it is in a proper format using Vitest and c8 for coverage
+- e2e test, if required, to make sure the app behaves as intended
+- Implement Loading state just in case and Error states/messages to show to a user, if something goes wrong
+- Divide fetch logic into utility-first approach. You can find a comprehensive example of how I did it in another project by going through [this link](https://github.com/hotepp/online-santa)
+- Moving all text and static data into `.json` files in a separate folder to be able to translate it and change with ease. Potentially, move it to a Headless CMS
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,41 +7,43 @@ const Grid = ({ coffee, tea, active }) => {
   return (
     <section className="container">
       <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {items.map((item) => (
-          <li
-            className="rounded-lg border border-border bg-white p-4"
-            key={item._id}
-          >
-            <Image
-              className="mx-auto"
-              src={item.image}
-              width="248"
-              height="248"
-              alt={item.name}
-            />
-            <div className="mt-4 flex justify-between gap-4 border-t border-border pt-[9px]">
-              <h2 className="text-lg font-semibold leading-[22px]">
-                {item.name}
-              </h2>
-              {item.tags && (
-                <ul className="flex flex-col gap-1">
-                  {item.tags.map((tag) => (
-                    <li
-                      className={clsx(
-                        "rounded-sm px-2 py-1 text-[13px] font-semibold uppercase leading-4 text-white",
-                        tag == "Arabic" && `bg-[#CD4C15]`,
-                        tag == "Robusta" && `bg-[#758266]`
-                      )}
-                      key={tag}
-                    >
-                      {tag}
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          </li>
-        ))}
+        {items &&
+          items.length &&
+          items.map((item) => (
+            <li
+              className="rounded-lg border border-border bg-white p-4"
+              key={item._id}
+            >
+              <Image
+                className="mx-auto"
+                src={item.image}
+                width="248"
+                height="248"
+                alt={item.name}
+              />
+              <div className="mt-4 flex justify-between gap-4 border-t border-border pt-[9px]">
+                <h2 className="text-lg font-semibold leading-[22px]">
+                  {item.name}
+                </h2>
+                {item.tags && (
+                  <ul className="flex flex-col gap-1">
+                    {item.tags.map((tag) => (
+                      <li
+                        className={clsx(
+                          "rounded-sm px-2 py-1 text-[13px] font-semibold uppercase leading-4 text-white",
+                          tag == "Arabic" && `bg-[#CD4C15]`,
+                          tag == "Robusta" && `bg-[#758266]`
+                        )}
+                        key={tag}
+                      >
+                        {tag}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            </li>
+          ))}
       </ul>
     </section>
   );
